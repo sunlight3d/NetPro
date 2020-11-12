68530fecb1ceba0fabdf406b433846fa01fc2c62
Nguyen Duc Hoang <sunlight4d@gmail.com>
2020-11-12 13:20:25 +0700

    update

---
+++ MCSDThang10-2020/12-11-2020/02_StateExample_begin/StateExample/Controllers/AnotherController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StateExample.Controllers
+{
+    public class AnotherController : Controller
+    {
+        public IActionResult Index()
+        {
+            int? overallVisitsNumber = HttpContext.Session.GetInt32("Overall");
+            int? controllerVisitsNumber = HttpContext.Session.GetInt32("Another");
+            if (overallVisitsNumber == null)
+            {
+                overallVisitsNumber = 1;
+            }
+            else
+            {
+                overallVisitsNumber++;
+            }
+            if (controllerVisitsNumber == null)
+            {
+                controllerVisitsNumber = 1;
+            }
+            else
+            {
+                controllerVisitsNumber++;
+            }
+            HttpContext.Session.SetInt32("Overall", overallVisitsNumber.Value);
+            HttpContext.Session.SetInt32("Another", controllerVisitsNumber.Value);
+            return View();
+        }
+    }
+}
---
+++ MCSDThang10-2020/12-11-2020/03_SignalRExample_begin/SignalRExample/Hubs/SquaresHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalRExample.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRExample.Hubs
+{
+    public class SquaresHub:Hub
+    {
+        private ISquareManager _manager;
+
+        public SquaresHub(ISquareManager manager)
+        {
+            _manager = manager;
+        }
+
+        public async Task SwapColor(int rowIndex, int columnIndex)
+        {
+            _manager.SwapColor(rowIndex, columnIndex);
+            await Clients.Others.SendAsync("SwapSquareColor", rowIndex, columnIndex);
+            //server gui cho cac client
+        }
+    }
+}
